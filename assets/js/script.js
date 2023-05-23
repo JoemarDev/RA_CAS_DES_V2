dde0c40d85cac15511e63d441f3661092af445cf
joemardev <joemarb.dev@gmail.com>
2023-05-23 10:32:11 +0800

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,42 @@ $('.brand').click(() => {
     window.location.href = "index.html"
 });
 
+
+const ToogleGameTabMain = (val) => {
+    $('.links').removeClass('active')
+    $('.game-container').hide();
+
+    if (val === 1) {
+        $(".live-box").show()
+        $(".live").addClass('active')
+        $('html, body').animate({
+            scrollTop: $(".live").offset().top
+        }, 600);
+
+    }
+    if (val === 2) {
+        $(".slot-box").show()
+        $(".slot").addClass('active')
+        $('html, body').animate({
+            scrollTop: $(".slot").offset().top
+        }, 600);
+    }
+    if (val === 3) {
+        $(".sports-box").show()
+        $(".sports").addClass('active')
+        $('html, body').animate({
+            scrollTop: $(".sports").offset().top
+        }, 600);
+    }
+    if (val === 4) {
+        $(".arcade-box").show()
+        $(".arcade").addClass('active')
+        $('html, body').animate({
+            scrollTop: $(".arcade").offset().top
+        }, 600);
+    }
+}
+
 const ToogleGameTab = (val) => {
     $('.links').removeClass('active')
     // $('.game-container').hide();
